9cf81bd634e93d0a63103d99752d5cf9020bbd0d
Gem <gedu07@gmail.com>
2012-10-10 11:04:23 -0300

    add 1 toast

--- MainForm/src/gem/test/mainform/ContWind.java
+++ MainForm/src/gem/test/mainform/ContWind.java
@@ -17,8 +17,10 @@ public class ContWind extends Activity{
 		TextView viewName = (TextView)findViewById(R.id.contName);
 		TextView viewSurname = (TextView)findViewById(R.id.contSurname);
 		//TextView viewSex = (TextView)findViewById(R.id.sex);
-		Toast toast1 = Toast.makeText(getApplicationContext(),"Hi there!",Toast.LENGTH_SHORT);
-		toast1.show();
+		Toast toast3 = Toast.makeText(getApplicationContext(),"Hi there!",Toast.LENGTH_SHORT);
+		toast3.show();
+		Toast toast2 = Toast.makeText(getApplicationContext(),"Helo!",Toast.LENGTH_SHORT);
+		toast2.show();
 		Bundle bndInf = getIntent().getExtras();
 		Log.d("ContWind","name: "+bndInf.getString("NAME"));
 		viewName.setText("Tu nombre es: "+bndInf.getString("NAME"));
